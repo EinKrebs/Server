df6b3f3b33b97b5a526a1e3593e1d3eb14ac2a43
Yury Frolov <yura200253@gmail.com>
2020-11-21 22:17:49 +0500

    Added server test & some fixes

--- test_all.py
+++ test_all.py
@@ -1,9 +1,15 @@
 import unittest
 
+# noinspection PyUnresolvedReferences
 from tests.request_tests import HttpRequestTests
+# noinspection PyUnresolvedReferences
 from tests.response_tests import HttpResponseTests
+# noinspection PyUnresolvedReferences
 from tests.text_tests import TextTests
+# noinspection PyUnresolvedReferences
 from tests.infracstructure_tests import IteratorExtensionsTests
+# noinspection PyUnresolvedReferences
+from tests.server_tests import ServerTests
 
 
 if __name__ == '__main__':
